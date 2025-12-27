edaff10d68f78866c201189418f7649f8f810351
Avery <jholmesoliver@gmail.com>
2025-12-28 11:31:17 +1300

    Added Battery status in bar.sh

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -1,19 +1,50 @@
 #!/bin/sh
-date_formatted=$(date "+%d/%m/%Y %H:%M")
-root_percent=$(df --output=pcent / | tail -n 1 | tr -d ' %')
-network_name=$(nmcli -t -f NAME c show --active | head -n 1)
-connection=$(nmcli -t -f CONNECTIVITY general)
-base_color='\e[0m'
+DATE_FORMATTED=$(date "+%d/%m/%Y %H:%M")
+ROOT_PERCENT=$(df --output=pcent / | tail -n 1 | tr -d ' %')
+NETWORK_NAME=$(nmcli -t -f NAME c show --active | head -n 1)
+CONNECTION=$(nmcli -t -f CONNECTIVITY general)
+BASE_COLOR='\e[0m'
+CONNECTION_COLOR=""
 
 ## if statement for connection as well as percentage of root used 
-if [ "$connection"="full" ] 
+if [ "$CONNECTION"="full" ];
 then
-				connection_color='\e[32m'
+				CONNECTION_COLOR='\e[32m'
+elif [ '$CONNECTION'='none' ];
+then
+				CONNECTION_COLOR='\e[31m'
 fi
 
-if [ '$connection'='none' ]
-then
-				connection_color='\e[31m'
+# Getting the data and initializing an array.
+BATTERY_INFO=($( acpi | awk -F',' '{ print $0 }'))
+
+# Formatting helpers
+CHARGE=$((${BATTERY_INFO[3]//%,}))
+ICON=""
+FORMAT=""
+
+# Format battery icon, depending on the status.
+if [[ "${BATTERY_INFO[2]}" == *"Charging"* ]]; then
+    ICON="  " # Plug icon, font awesome.
+else
+    ICON=" " # Car Battery icon, font awesome
 fi
-echo " $network_name | /: $root_percent% used | $date_formatted "
+
+# Format charge & color depending on the status.
+if [[ $CHARGE -lt 30 ]]; then
+    # Red-ish
+    FORMAT="\e[1;31m"
+elif [[ $CHARGE -lt 60 ]]; then
+    # Yellow-ish
+    FORMAT="\e[1;33m"
+elif [[ $CHARGE -lt 100 ]]; then
+    # Green-ish
+    FORMAT="\e[1;32m"
+fi
+
+BAT_STATUS=$"${ICON}${FORMAT}${CHARGE}%" 
+
+# Final formatted output.
+echo -e " $CONNECTION_COLOR$NETWORK_NAME \e[0m| $BAT_STATUS \e[0m|/: $ROOT_PERCENT% used | $DATE_FORMATTED "
 #echo -e "${connection_color} $network_name ${base_color}| /: $root_percent% used | $date_formatted "
+echo $CONNECTION
