750fcbe8b93cfd12b173bb18b0b96b0ff1faa568
Avery <jholmesoliver@gmail.com>
2026-01-05 10:32:53 +1300

    Simplified down swaybar script again. To be just icons and texts. No colors but still all info

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -1,30 +1,24 @@
 #!/bin/sh
-
-# Defining color pallete
-RED="#FF5555"
-YELLOW="#F1FA8C"
-GREEN="#50FA7B"
-WHITE="#F8F8F2"
-BLUE="#8BE9FD"
-
-
 DATE=$(date "+%d/%m/%Y %H:%M")
 
 # Module for network
 NETWORK_NAME=$(nmcli -t -f NAME c show --active | head -n 1)
+RADIO_WIFI=$(nmcli radio wifi)
 CONNECTION=$(nmcli -t -f CONNECTIVITY general)
-CONNECTION_COLOR=""
+CONNECTION_ICON=""
 
 #echo -e "${connection_color} $network_name ${base_color}| /: $root_percent% used | $date_root_colorted "
 if [[ "$CONNECTION" == *"full"* ]]; then
-				CONNECTION_COLOR=$GREEN
+				CONNECTION_ICON=" "
+elif [[ "$RADIO_WIFI" == *"disabled" ]]; then
+				CONNECTION_ICON="󰀝"
+				NETWORK_NAME="Airplane mode"
 else
-				CONNECTION_COLOR=$RED
 				NETWORK_NAME="None"
+				CONNECTION_ICON="󰖪"
 fi
 
-NET_STATUS=$"$CONNECTION_COLOR$NETWORK_NAME$WHITE"
-#NET_STATUS=$"$CONNECTION_COLOR$NETWORK_NAME"
+NET_STATUS="$CONNECTION_ICON  $NETWORK_NAME"
 
 
 # Getting the data and initializing an array.
@@ -43,40 +37,19 @@ else
 fi
 
 # Format charge & color depending on the status.
-if [[ $CHARGE -lt 30 ]]; then
-    # red-ish
-    BAT_STATUS=$RED
-elif [[ $CHARGE -lt 60 ]]; then
-    # yellow-ish
-    BAT_STATUS=$YELLOW
-elif [[ $CHARGE -lt 100 ]]; then
-    # green-ish
-    BAT_STATUS=$GREEN
-fi
-
-BAT_STATUS=$"${ICON}${BAT_STATUS}${CHARGE}%$WHITE" 
+BAT_STATUS=$"${ICON}${BAT_STATUS}${CHARGE}%" 
 #BAT_STATUS=$"${ICON}${BAT_STATUS}${CHARGE}%" 
 
 # Root Stats
 ROOT_PERCENT=$(df --output=pcent / | tail -n 1 | tr -d ' %')
-ROOT_COLOR=""
-# Color root based on percent used is opposite of Battery 
-if [[ $ROOT_PERCENT -lt 30 ]]; then
-    ROOT_COLOR=$GREEN
-elif [[ $ROOT_PERCENT -gt 50 ]]; then
-    ROOT_COLOR=$YELLOW
-elif [[ $ROOT_PERCENT -gt 75 ]]; then
-    ROOT_COLOR=$RED
-fi
 
-ROOT_STATUS=$"$ROOT_COLOR$ROOT_PERCENT% used$WHITE"
-#ROOT_STATUS="$ROOT_COLOR$ROOT_PERCENT% used"
+
+ROOT_STATUS=$"$ROOT_PERCENT% used"
 
 # Volume Module
 VOLUME=$"$(pamixer --get-volume)"
 MUTED=$"$(pamixer --get-mute)"
 VOLUME_ICON=""
-VOLUME_COLOR=""
 
 #Different icons based on if muted or not
 if [[ "$MUTED" == *"true"* ]]; then
@@ -85,54 +58,32 @@ else
 				VOLUME_ICON="󰕾"
 fi
 
-# Color based on volume amount greater than 80 likely quite loud
-if [[ $VOLUME = 0 || $VOLUME -gt 80 ]]; then
-				VOLUME_COLOR=$RED
-elif [[ "$MUTED" == *"true"* ]]; then
-				VOLUME_COLOR=$YELLOW
-else
-				VOLUME_COLOR=$WHITE
-fi
 
-VOLUME_STATUS="$VOLUME_ICON $VOLUME_COLOR$VOLUME% $WHITE"
-#VOLUME_STATUS="$VOLUME_ICON $VOLUME_COLOR$VOLUME%"
+VOLUME_STATUS="$VOLUME_ICON $VOLUME%"
 
 # Brightness Module
 BRIGHTNESS=$(brightnessctl info | awk -F'[()%]' '/Current brightness/ {print $2}')
 BRIGHTNESS_ICON="󰃠"
-BRIGHTNESS_COLOR=""
-
-# If brightness greater than 70 likely to drain battery faster
-if [[ $BRIGHTNESS -gt 70 ]]; then
-				BRIGHTNESS_COLOR=$RED
-else
-				BRIGHTNESS_COLOR=$WHITE
-fi
 
 BRIGHTNESS_STATUS="$BRIGHTNESS_ICON $BRIGHTNESS"
 # Bluetooth Module
-BLUETOOTH="" # Bluetoothctl get status or something
+BLUETOOTH=$(systemctl is-active "bluetooth.service") # Bluetoothctl get status or something
 BLUETOOTH_ICON=""
-BLUETOOTH_COLOR=""
 # If statement based on status
-if [[ $BLUETOOTH_STATUS ]]; then
+if [[ $BLUETOOTH ]]; then
 				BLUETOOTH_ICON="󰂯"
 
 else
 				BLUETOOTH_ICON="󰂲"
 fi
 
-if [[ $BLUETOOTH_STATUS ]]; then
-				BLUETOOTH_COLOR=$BLUE
-
-else
-				BLUETOOTH_COLOR=$RED
-fi
-
 BLUETOOTH_STATUS="$BLUETOOTH_ICON $BLUETOOTH"
 # Final formated output.
-# echo "[{"full_text":"$NETWORK_STATUS","color":"$NETWORK_COLOR"},{"full_text":"$ROOT_STATUS","color":"$ROOT_COLOR"},{"full_text":"$BRIGHTNESS_STATUS","color":"BRIGHTNESS_COLOR"},{"full_text":"$VOLUME_STATUS","color":"$VOLUME_COLOR"},{"full_text":"$DATE","color":"$WHITE"}]"
-# Need to use better protocol that complies with it. Use block function and parameters.
+
+# Laptop statusline
+# echo "$NET_STATUS | $BLURTOOTH_STATUS | $VOLUME_STATUS | $BRIGHTNESS_STATUS | $BAR_STATUS |$ROOT_PERCENT% of root used | $DATE"
+ 
 
 # Simplified status icons
-echo $"$NETWORK_NAME | $ROOT_PERCENT% used | $DATE"
+echo "$NET_STATUS | $ROOT_PERCENT% of root used | $DATE"
+
